ad9ed804b96b606d2ae20aec683b06b35afd65e3
Jake <jlvgonzales@universityofbohol.edu.ph>
2026-02-24 13:07:30 +0800

    Add files via upload

---
+++ Activity 2 (unfinished).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int elements[5];
+int count = 0;
+
+
+void add() {
+    
+  
+    
+    for(int i = 0; i < 5; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &elements[i]);
+    }
+    
+}    
+
+void find() {
+    
+    
+    printf("Enter the element to search: ");
+    scanf("%d", &elements);
+    
+    
+    while i < count && found == false
+    
+    
+}
+    
+  
