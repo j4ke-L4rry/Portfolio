d59b57036c316215e8051e542674807422540949
Jake <jlvgonzales@universityofbohol.edu.ph>
2026-02-26 13:29:40 +0800

    Update Activity 2 (unfinished).c

--- Activity 2 (unfinished).c
+++ Activity 2 (unfinished).c
@@ -1,29 +1,61 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int elements[5];
-int count = 0;
+   //1
+   int array[5], element;
+   int count = 0;
+   //2
+   int target, position;
+ bool found;
 
+//algorithm 1
 
 void add() {
     
+ 
   
     
     for(int i = 0; i < 5; i++) {
+        
         printf("Enter element %d: ", i + 1);
-        scanf("%d", &elements[i]);
+        scanf("%d", &element);
+        array[count] = element;
+        count++;
+     
     }
+
+    printf("5 elements have been successfully added.\n");
     
 }    
 
+
+//algorithm 2
+
 void find() {
-    
-    
+
     printf("Enter the element to search: ");
-    scanf("%d", &elements);
-    
-    
-    while i < count && found == false
-    
+    scanf("%d", &target);
+
+    found = false;
+    position = -1;
+
+    int i = 0;
+
+    while (i < count && found == false) {
+        if (array[i]==target) {
+            found = true;
+            position = i;
+        }
+        else { 
+            i++;
+        }
+    }
+    if (found == true) {
+        printf("Element %d found at position %d.\n", target, position);
+    }
+    else {
+        printf("Element %d not found in the array.\n", target);
+    }    
     
 }
     
