e7dc64e84e6afe6119666d7d404cb36173f2ddd8
Jake <jlvgonzales@universityofbohol.edu.ph>
2026-03-03 13:26:34 +0800

    Implement delete and sort functions
    Added delete and sort functions for array management.

--- Activity 2 (unfinished).c
+++ Activity 2 (unfinished).c
@@ -7,6 +7,10 @@
    //2
    int target, position;
  bool found;
+    //3
+    int pos;
+    //4
+    char choice;
 
 //algorithm 1
 
@@ -26,6 +30,7 @@ void add() {
 
     printf("5 elements have been successfully added.\n");
     
+
 }    
 
 
@@ -55,8 +60,130 @@ void find() {
     }
     else {
         printf("Element %d not found in the array.\n", target);
-    }    
+    } 
+    
+    
     
 }
+
+
+    //algorithm 3
+
+
+    void delete() {
+        //step 2
+        if (count == 0) {
+            printf("Array is empty. Nothing to delete.");
+        }
+        //step 3 and step 4
+        printf("Enter the element to delete.");
+        scanf("%d", &target);
+        //step 5
+        found == false;
+
+        for (int i=0; i < count; i++) {
+            if (array[i]==target){
+            pos = i;
+            found = true;
+            break; //to stop loop
+            }
+        }
+        //step 6
+        if (found == false){
+            printf("Element not found. No deletion made.\n");
+            return;
+        }
+        //step 7
+        for(int i = pos; i < count - 1; i++){
+            array[i] = array [i + 1];
+        }
+        //step 8 decrement 
+        count--;
+
+
+    }
+
+    //algorithm 4
+
+
+    void sort() {
+        int temp;
+        bool swapped;
+        //step 2
+        if(count < 2){
+            printf("Not enough elements to sort.");
+            break;
+        }
+        //step 3 and step 4
+        printf("Display: Sort in (A) Ascending or (D) Descending order?");
+        scanf(" %c", &choice);
+        
+        //step 5
+        for(i=0; i < count; i++) {
+            swapped=false;
+            for(j=0; j < count - 2 - i; j++){
+                if(choice == 'A' && array[j]>array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    swapped = true;
+
+                }
+                else if(choice == 'D' && array[j] < array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+        //step 6
+        if(choice = 'A'){
+            printf("Array sorted in Ascending order.");
+        }
+        //step 7
+        else if(choice ='D'){
+            printf("Array sorted in Descending order.");
+        }
+        //step 8
+        else {
+            printf("Invalid choice. Returning to menu.");
+        }
+
+        return 0;
+
+    }
+
+    //Algorithm 5
+
+    void Print(){
+        //step 2
+        if(count==0){
+            printf("The array is empty.");
+            break;
+        }
+        //step 3
+        printf("Current elements in the array: ");
+        //step 4
+        for(i=0; i < count; i++){
+            printf("Position %d: %d\n");
+        }
+        //step 5
+        printf("Total elements: [%d]", count);
+    }
+
+    int main(){
+
+
+        add();
+
+    }
+
     
   
