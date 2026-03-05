e7f5616eb04958662af2b9783aeb01cb5f513d74
Jake <jlvgonzales@universityofbohol.edu.ph>
2026-03-05 13:19:06 +0800

    Add binary search option to find function

--- Activity 2 (unfinished).c
+++ Activity 2 (unfinished).c
@@ -36,8 +36,17 @@ void add() {
 
 void find() {
 
+    int num;
+
     printf("Enter the element to search: ");
     scanf("%d", &target);
+
+
+    Option:
+    printf("Choose 1 for Linear search and 2 for Binary search: ");
+    scanf("%d", &num);
+
+    if (num == 1) {
 
     found = false;
     position = -1;
@@ -59,6 +68,40 @@ void find() {
     else {
         printf("Element %d not found in the array.\n", target);
     } 
+
+ }
+
+    else if (num == 2) {
+
+                    int low = 0, high = count - 1, mid;
+                    int found = 0;
+
+
+                    while(low <= high){
+                        mid = (low + high) / 2;
+
+                        if(array[mid] == target){
+                            printf("Element found at position %d\n", mid + 1);
+                            found = 1;
+                            break;
+                        }
+                        else if(array[mid] < target)
+                            low = mid + 1;
+                        else
+                            high = mid - 1;
+                    }
+
+                    if(!found)
+                        printf("Element not found in array.\n");
+
+    
+                }
+
+      else {
+        printf("Invalid choice\n");
+        goto Option;
+      }         
+    
     
     
     
@@ -110,11 +153,11 @@ void find() {
         bool swapped;
         //step 2
         if(count < 2){
-            printf("Not enough elements to sort.");
+            printf("Not enough elements to sort.\n");
             return;
         }
         //step 3 and step 4
-        printf("Display: Sort in (A) Ascending or (D) Descending order?");
+        printf("Display: Sort in (A) Ascending or (D) Descending order?\n");
         scanf(" %c", &choice);
         
         //step 5
@@ -144,15 +187,15 @@ void find() {
         }
         //step 6
         if(choice == 'A'){
-            printf("Array sorted in Ascending order.");
+            printf("Array sorted in Ascending order.\n");
         }
         //step 7
         else if(choice =='D'){
-            printf("Array sorted in Descending order.");
+            printf("Array sorted in Descending order.\n");
         }
         //step 8
         else {
-            printf("Invalid choice. Returning to menu.");
+            printf("Invalid choice. Returning to menu.\n");
         }
 
     
@@ -164,16 +207,16 @@ void find() {
     void Print(){
         //step 2
         if(count==0){
-            printf("The array is empty.");
+            printf("The array is empty.\n");
         }
         //step 3
-        printf("Current elements in the array: ");
+        printf("Current elements in the array: \n");
         //step 4
         for(int i=0; i < count; i++){
             printf("Position %d: %d\n", i + 1, array[i]);
         }
         //step 5
-        printf("Total elements: [%d]", count);
+        printf("Total elements: [%d]\n", count);
     }
 
     int main(){
@@ -181,7 +224,7 @@ void find() {
         int choose;
 
     while (1) {
-       printf("Menu of operations:\n1. Add 5 elements to the array\n2. Search for an element in the array\n3. Delete an element from the array\n4. Sort the elements (Ascending or Descending)\n5. Print the final elements in the array\n");
+       printf("Menu of operations:\n1. Add 5 elements to the array\n2. Search for an element in the array\n3. Delete an element from the array\n4. Sort the elements (Ascending or Descending)\n5. Print the final elements in the array\n6. Exit the Program\n");
        printf("Enter your choice: \n");
        scanf("%d", &choose);
 
